a77a3618403ab15614443abc95c88e90a01ba07b
Mike Curtin <curtinmjc@gmail.com>
2018-10-04 13:44:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // add solution here
+function theBeatlesPlay(musiciansArray, instrumentsArray) {
+  var array = [];
+  
+  for (var i = 0; i < musiciansArray.length; i++) {
+    array.push(musiciansArray[i] + ' plays ' + instrumentsArray[i]);
+  }
+  
+  return array;
+}
+
+function johnLennonFacts(factsArray) {
+//  newArray = [];
+  var i = 0;
+  while (i < factsArray.length) {
+    factsArray[i] += '!!!';
+    i++;
+  }
+  return factsArray;
+}
